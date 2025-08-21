7a6797ad8f30d35d181a340b8d868e33264dfc46
arif hidayat <arifhidayatbiz@gmail.com>
2025-08-21 13:40:59 +0700

    remove node_modules

--- dist/command_exit.js
+++
@@ -1,5 +0,0 @@
-export async function commandExit() {
-    console.log("Closing the Pokedex... Goodbye!");
-    process.exit(0);
-}
-;
--- dist/command_help.js
+++
@@ -1,8 +0,0 @@
-export async function commandHelp() {
-    console.log("Welcome to the Pokedex!");
-    console.log("Usage:");
-    console.log("");
-    console.log("help: Displays a help message");
-    console.log("exit: Exit the Pokedex");
-}
-;
